121e11257457e914d44a306f858b45cfc9be9dc3
apple <ssv6132@gmail.com>
2026-05-08 00:39:12 +0530

    init setup for the backend server api and deployement setup

---
+++ backend/app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(title="Post morph API")
+
+@app.get("/")
+def read_root():
+	return {"Hello": "Man" }
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None): 
+	return {"item_id": item_id, "q": q}
